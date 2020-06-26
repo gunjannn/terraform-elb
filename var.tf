2ddb8c52f9c5909d3d12294febffc05f1ec87776
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-06-26 10:09:05 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,24 +1,24 @@
 variable "aws_vpc" {
     description = "VPC"
-    default = "vpc-6d8f7507"
+    default = "vpc-d4292cae"
 }
 
 variable "aws_subnet" {
     description = "aws subnet"
-    default = "subnet-29903243"
+    default = "subnet-026e523c"
 }
 
-variable "aws_subnet2" {
+/*variable "aws_subnet2" {
     description = "aws subnet2"
     default = "subnet-81b9a0fc"
-}
+}*/
 
 variable "aws_key" {
     description = "aws key"
-    default = "DevOpsKey"
+    default = "DevOps"
 }
 
  variable "aws_ami" {
      description = "ami id"
-     default = "ami-076431be05aaf8080"
+     default = "ami-09d95fab7fff3776c)"
 }
