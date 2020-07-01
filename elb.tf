2ff36cb79212ba4730b60f4ac03082be2f9df587
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-02 01:51:04 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -13,7 +13,7 @@ resource "aws_lb_target_group" "target-grp" {
   name     = "target-grp"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = ["${var.aws_vpc}"]
+  vpc_id   = "var.aws_vpc"
   health_check {
                 path = "/healthcheck"
                 port = "80"
@@ -45,7 +45,7 @@ resource "aws_lb_target_group_attachment" "target2" {
 #  }
 
 resource "aws_lb_listener" "elb_listener" {
-  load_balancer_arn = "${aws_lb.http_elb.arn}"
+  load_balancer_arn = ["${aws_lb.http_elb.arn}"]
   port              = "80"
   protocol          = "HTTP"
   default_action {
