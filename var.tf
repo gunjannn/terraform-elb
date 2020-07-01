74b83056e7a04def7cafab4c9eda91febeba19d8
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-02 01:17:04 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -8,10 +8,10 @@ variable "aws_subnet" {
     default = "subnet-026e523c"
 }
 
-/*variable "aws_subnet2" {
+variable "aws_subnet2" {
     description = "aws subnet2"
     default = "subnet-81b9a0fc"
-}*/
+}
 
 variable "aws_key" {
     description = "aws key"
@@ -20,5 +20,11 @@ variable "aws_key" {
 
  variable "aws_ami" {
      description = "ami id"
-     default = "ami-09d95fab7fff3776c)"
+     default = "ami-09d95fab7fff3776c"
+}
+
+
+ variable "sec-grp" {
+     description = "sec grp"
+     default = "sg-540a0f78"
 }
