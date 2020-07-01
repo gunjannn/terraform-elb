865a2e70a9fd997e9367c75aab1a33b71bf7b5dd
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-02 03:38:04 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "http_elb" {
   name               = "http-elb"
   internal           = false
   load_balancer_type = "application"
-  security_groups   = "aws_security_group.default_security_group.id"      
+  /*security_groups   = "aws_security_group.default_security_group.id"*/      
   subnets            = [
     "var.aws_subnet",
     "var.aws_subnet2",
