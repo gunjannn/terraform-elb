b4f49738be14d5ff3a3cacc9053478d8b35b558f
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-02 02:02:23 +0530

    Update linux_vm2.tf

--- linux_vm2.tf
+++ linux_vm2.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "linux_vm2" {
-    ami                         = "${var.aws_ami}"
+    ami                         = "var.aws_ami"
     availability_zone           = "us-east-1b"
     ebs_optimized               = false
     instance_type               = "t2.micro"
     monitoring                  = false
-    key_name                    = "${var.aws_key}"
-    subnet_id                   = "${var.aws_subnet2}"
+    key_name                    = "var.aws_key"
+    subnet_id                   = "var.aws_subnet2"
     source_dest_check = false
-    vpc_security_group_ids      = ["${aws_security_group.sec-grp.id}"]
+    vpc_security_group_ids      = "aws_security_group.sec-grp.id"
     associate_public_ip_address = true
-    user_data = "${file("user-data2.sh")}"
+    user_data = "file("user-data2.sh")"
     root_block_device {
         volume_type           = "gp2"
         volume_size           = 8
