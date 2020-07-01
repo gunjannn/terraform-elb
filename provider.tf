963caeb47e4bb8c003a25487a194d10c0df4924e
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-02 00:46:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "eu-central-1"
-  access_key = "AKIA46G6QQAPZW6FYT5A"
-  secret_key = "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"
+  region     = "us-east-1"
+  access_key = "AKIA46G6QQAPRK44V3G3"
+  secret_key = "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"
 }
