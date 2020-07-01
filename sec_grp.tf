5331a9cd4d36978ea21f99a07eb3d29e34cc57ba
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-02 02:03:16 +0530

    Update sec_grp.tf

--- sec_grp.tf
+++ sec_grp.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "sec-grp" {
 
     name = "sec-grp"
 
-    vpc_id =  "${var.aws_vpc}"
+    vpc_id =  "var.aws_vpc"
 
     tags = {
         Name = "sec-grp"
@@ -16,7 +16,7 @@ resource "aws_security_group_rule" "allow-ingress" {
         to_port         = "0"
         protocol        = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-        security_group_id = "${aws_security_group.sec-grp.id}"
+        security_group_id = "aws_security_group.sec-grp.id"
 }
 
 resource "aws_security_group_rule" "allow-egress" {
@@ -26,6 +26,6 @@ resource "aws_security_group_rule" "allow-egress" {
         to_port         = "0"
         protocol        = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-        security_group_id = "${aws_security_group.sec-grp.id}"
+        security_group_id = "aws_security_group.sec-grp.id"
 }
 
